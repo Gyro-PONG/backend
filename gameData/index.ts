8969d30815b6dc76e5bcfddb12d94ea416173be4
dongtimes2 <dongtimes2@gmail.com>
2023-12-10 20:40:39 +0900

    feat: 게임 데이터에 관한 객체 정보 저장 파일 구현

---
+++ gameData/index.ts
@@ -0,0 +1,5 @@
+import { GameList } from '../class/GameList.js';
+import { UserList } from '../class/UserList.js';
+
+export const userList = new UserList();
+export const gameList = new GameList();
